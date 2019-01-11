3ffc55740adff5adb93a0f29fb1beffe323944d7
Tom Dohnal <tomas.dohnal@gmail.com>
2019-01-11 20:24:24 +0100

    use custom auth middleware

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use App\User;
+use Tymon\JWTAuth\Token;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
-class Authenticate extends Middleware
+class Authenticate
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return string
+     * @param  \Closure  $next
+     * @return mixed
      */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (!auth()->user()) {
+            throw new \Exception('You must authenticate in order to proceed.');
         }
+        return $next($request);
     }
 }
