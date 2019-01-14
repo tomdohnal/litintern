7b00ac370e09160da777eca0d1973cd1e70f555f
Tom Dohnal <tomas.dohnal@gmail.com>
2019-01-14 12:53:20 +0100

    fix FOUC

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,14 +1,39 @@
-import Document, { Main, NextDocumentContext, NextScript } from 'next/document'
+import Document, {
+  Head,
+  Main,
+  NextDocumentContext,
+  NextScript,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: NextDocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const page = ctx.renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />),
+    )
+    const styleTags = sheet.getStyleElement()
+
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    return {
+      ...initialProps,
+      ...page,
+      styleTags,
+    }
   }
 
   render() {
     return (
       <html>
+        <Head>
+          {this.props.styleTags}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+          <style>{`body { margin: 0; background: #F8F8F8 } a { text-decoration: none}`}</style>
+          <title>Lit Intern</title>
+        </Head>
         <body>
           <Main />
           <NextScript />
